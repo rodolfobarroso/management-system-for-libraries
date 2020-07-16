1e7f3f66f430f75988de626ab5df74aa4df469e9
RodolfoBarroso <rodolfocesar24@gmail.com>
2020-07-15 23:06:50 -0300

    Adiciona o DBcontext

---
+++ ManagementSystemforLibraries/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ManagementSystemforLibraries.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagementSystemforLibraries.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {}
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<AuthorBook> AuthorBooks { get; set; }
+        public DbSet<PublishingCompany> PublishingCompanies { get; set; }
+        public DbSet<Copy> Copies { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthorBook>().HasKey(authorBook => new { authorBook.Id });
+        }
+    }
+}
